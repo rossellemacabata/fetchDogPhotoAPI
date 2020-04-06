d506f2ce19e0c2116f21cb3124cc45de23cef1a5
ZellieMacabata <rossellemacabata@gmail.com>
2020-04-05 17:58:56 -0700

    Completing coding challenge to fetch random dog photos API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogPhotosData: []
+    };
+  }
+
+  fetchData = async () => {
+  
+    const result = await axios.get(`https://dog.ceo/api/breeds/image/random`);
+    this.setState(state => {
+      const dogPhotosData = state.dogPhotosData.concat(result.data.message);
+      return {
+        dogPhotosData
+      }
+
+    })    
+  };
+
+  render() {
+  
+    return (
+      <div id="container">
+        <button className="fetch" onClick={this.fetchData}>FETCH</button>
+        {this.state.dogPhotosData.map((photo, index) => {
+          return(
+            <div className="gallery"><img src={photo}></img></div>
+          )
+        })}
+   
+      </div>
+    );
+  }
 }
 
 export default App;
